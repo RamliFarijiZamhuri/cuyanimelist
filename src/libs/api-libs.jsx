23645d95ae5959e639cd9ab86bba4de5d6116ce3
ramli fariji zamhuri <ramlif828@gmail.com>
2023-11-02 20:54:20 +0700

    hayo

--- src/libs/api-libs.jsx
+++ src/libs/api-libs.jsx
@@ -9,7 +9,7 @@ export const getNestedAnimeResponse = async(resource, objectProperty) => {
     return response.data.flatMap(item => item[objectProperty])
 }
 
-export const reproduce = (data, gap = 5)  => {
+export const reproduce = (data, gap = 8)  => {
     const first = ~~(Math.random() * (data.length - gap) + 1)
     const last = first + gap
 
