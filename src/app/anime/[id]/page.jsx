74d0cd1acf94869f1d7c1320a1154f8c38ff80d9
ramli fariji zamhuri <ramlif828@gmail.com>
2023-11-10 17:29:17 +0700

    lagi puh

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -7,41 +7,44 @@ const Page = async ({ params: { id } }) => {
 
     return (
         <>
-            <div className="pt-4 px-4">
-                <p className="text-2xl text-color-primary">{anime.data.title} = {anime.data.year}</p>
-            </div>
-            <div className="pt-4 px-4 flex gap-2 text-color-primary overflow-auto">
-                <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-3">
-                    <h3>Peringkat</h3>
-                    <p>{anime.data.rank}</p>
+            <div className="flex flex-col md:gap-3 gap-4 py-4 md:mx-4 mx-2">
+                <div className="pt-4 px-2">
+                    <p className="text-2xl text-color-primary">{anime.data.title} = {anime.data.year}</p>
                 </div>
-                <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-3">
-                    <h3>Skor</h3>
-                    <p>{anime.data.score}</p>
+                <div className="pt-4 flex gap-2 text-color-primary overflow-auto">
+                    <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-3">
+                        <h3>Peringkat</h3>
+                        <p>{anime.data.rank}</p>
+                    </div>
+                    <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-3">
+                        <h3>Skor</h3>
+                        <p>{anime.data.score}</p>
+                    </div>
+                    <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-3">
+                        <h3>Anggota</h3>
+                        <p>{anime.data.members}</p>
+                    </div>
+                    <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-3">
+                        <h3>Episode</h3>
+                        <p>{anime.data.episodes}</p>
+                    </div>
+                    <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-3">
+                        <h3>Favorit</h3>
+                        <p>{anime.data.favorites}</p>
+                    </div>
+                    <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-3">
+                        <h3>Popularitas</h3>
+                        <p>{anime.data.popularity}</p>
+                    </div>
                 </div>
-                <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-3">
-                    <h3>Anggota</h3>
-                    <p>{anime.data.members}</p>
+                <div className="grid md:grid-cols-2 gap-2 grid-cols-1">
+                    <Image src={anime.data.images.webp.image_url} alt={anime.data.images.jpg.image_url} width={350} height={350} className="w-full rounded object-cover max-h-96" />
+                    <VideoPlayer youtubeId={anime.data.trailer.youtube_id} />
+
                 </div>
-                <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-3">
-                    <h3>Episode</h3>
-                    <p>{anime.data.episodes}</p>
+                <div>
+                    <p className="text-color-primary ">{anime.data.synopsis}</p>
                 </div>
-                <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-3">
-                    <h3>Favorit</h3>
-                    <p>{anime.data.favorites}</p>
-                </div>
-                <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-3">
-                    <h3>Popularitas</h3>
-                    <p>{anime.data.popularity}</p>
-                </div>
-            </div>
-            <div className="pt-4 px-4 flex gap-2 sm:flex-nowrap flex-wrap text-color-primary">
-                <Image src={anime.data.images.webp.image_url} alt={anime.data.images.jpg.image_url} width={250} height={250} className="w-full rounded object-cover" />
-                <p className="text-justify text-xl">{anime.data.synopsis}</p>
-            </div>
-            <div className="">
-                <VideoPlayer youtubeId={anime.data.trailer.youtube_id} />
             </div>
         </>
     )
--- src/components/Utilities/VideoPlayer.jsx
+++ src/components/Utilities/VideoPlayer.jsx
@@ -6,19 +6,19 @@ import Youtube from "react-youtube"
 const VideoPlayer = ({ youtubeId }) => {
     const [isOpen, setIsOpen] = useState(true)
 
-    const handleVideoPlayer = () => {
-        setIsOpen((prevState) => !prevState)
-    }
+    // const handleVideoPlayer = () => {
+    //     setIsOpen((prevState) => !prevState)
+    // }
 
     const option = {
-        width: "300",
-        height: "250"
+        width: "100%",
+        height: "384"
     }
 
     const Player = () => {
         return (
-            <div className="fixed bottom-2 right-2">
-                <buttton onClick={handleVideoPlayer} className="text-color-primary float-right bg-color-secondary px-3 mb-1 cursor-pointer">X</buttton>
+            <div>
+                {/* <buttton onClick={handleVideoPlayer} className="text-color-primary float-right bg-color-secondary px-3 mb-1 cursor-pointer">X</buttton> */}
                 <Youtube videoId={youtubeId} onReady={(event) => event.target.pauseVideo()} opts={option} onError={() => alert("Vidio Rusak.")} />
             </div>
         )
