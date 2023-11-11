0b8f61b524e86912256769fca4a4ebe86c6e01a0
ramli fariji zamhuri <ramlif828@gmail.com>
2023-11-11 12:00:24 +0700

    sepuh baru belajar

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -9,40 +9,40 @@ const Page = async ({ params: { id } }) => {
         <>
             <div className="flex flex-col md:gap-3 gap-4 py-4 md:mx-4 mx-2">
                 <div className="pt-4 px-2">
-                    <p className="text-2xl text-color-primary">{anime.data.title} = {anime.data.year}</p>
+                    <p className="text-2xl text-color-primary">{anime.data.title} {anime.data.year}</p>
                 </div>
-                <div className="pt-4 flex gap-2 text-color-primary overflow-auto">
-                    <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-3">
-                        <h3>Peringkat</h3>
-                        <p>{anime.data.rank}</p>
-                    </div>
-                    <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-3">
-                        <h3>Skor</h3>
-                        <p>{anime.data.score}</p>
-                    </div>
-                    <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-3">
-                        <h3>Anggota</h3>
-                        <p>{anime.data.members}</p>
-                    </div>
-                    <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-3">
-                        <h3>Episode</h3>
-                        <p>{anime.data.episodes}</p>
-                    </div>
-                    <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-3">
-                        <h3>Favorit</h3>
-                        <p>{anime.data.favorites}</p>
-                    </div>
-                    <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-3">
-                        <h3>Popularitas</h3>
-                        <p>{anime.data.popularity}</p>
-                    </div>
+                <div className="flex justify-start items-center p-4 gap-1 text-xl font-bold overflow-auto">
+                    <span className="whitespace-nowrap rounded-full bg-color-primary px-2.5 py-0.5 text-sm text-color-accent">
+                        Skor #
+                        {anime.data.score}
+                    </span>
+                    <span className="whitespace-nowrap rounded-full bg-color-primary px-2.5 py-0.5 text-sm text-color-accent">
+                        Peringkat #
+                        {anime.data.rank}
+                    </span>
+                    <span className="whitespace-nowrap rounded-full bg-color-primary px-2.5 py-0.5 text-sm text-color-accent">
+                        Anggota #
+                        {anime.data.members}
+                    </span>
+                    <span className="whitespace-nowrap rounded-full bg-color-primary px-2.5 py-0.5 text-sm text-color-accent">
+                        Episode #
+                        {anime.data.episodes}
+                    </span>
+                    <span className="whitespace-nowrap rounded-full bg-color-primary px-2.5 py-0.5 text-sm text-color-accent">
+                        Favorit #
+                        {anime.data.favorites}
+                    </span>
+                    <span className="whitespace-nowrap rounded-full bg-color-primary px-2.5 py-0.5 text-sm text-color-accent">
+                        Popularitas #
+                        {anime.data.popularity}
+                    </span>
                 </div>
                 <div className="grid md:grid-cols-2 gap-2 grid-cols-1">
                     <Image src={anime.data.images.webp.image_url} alt={anime.data.images.jpg.image_url} width={350} height={350} className="w-full rounded object-cover max-h-96" />
                     <VideoPlayer youtubeId={anime.data.trailer.youtube_id} />
-
                 </div>
-                <div>
+                <div className="flex flex-col py-4 gap-2">
+                    <h2 className="text-xl text-color-primary">{anime.data.duration} | {anime.data.rating}</h2>
                     <p className="text-color-primary ">{anime.data.synopsis}</p>
                 </div>
             </div>
