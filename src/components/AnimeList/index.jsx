4c89063e5eb811d229e18f04aecb33fd4a4903b4
ramli fariji zamhuri <ramlif828@gmail.com>
2023-12-31 12:50:38 +0700

    background

--- src/components/AnimeList/index.jsx
+++ src/components/AnimeList/index.jsx
@@ -6,9 +6,9 @@ const AnimeList = ({ api }) => {
         <div className="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-2 px-4 gap-4">
             {api.data?.map((anime, index) => {
                 return (
-                    <Link href={`/anime/${anime.mal_id}`} className="bg-color-accent rounded-md cursor-ponter text-color-primary hover:text-color-accent transition-all" key={index}>
+                    <Link href={`/anime/${anime.mal_id}`} className="bg-color-primary rounded-md cursor-ponter text-color-dark hover:text-color-accent transition-all" key={index}>
                         <Image src={anime.images.webp.image_url} alt="...." width={350} height={350} className="w-4/5 m-auto mt-4 max-h-64 object-cover overflow-x-auto" />
-                        <h3 className="justify-between font-bold md:text-xl text-md p-1">{anime.title}</h3>
+                        <h3 className="justify-between m-auto font-bold md:text-xl text-md p-1">{anime.title}</h3>
                     </Link>
                 )
             })}
